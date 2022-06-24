f48e63c5933ba3736c4647a7b9d15e809e8e96ad
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-24 11:41:27 +0100

    fix style issues

--- src/navbar.js
+++ src/navbar.js
@@ -7,7 +7,7 @@ const renderNavbar = async () => {
   resultList.innerHTML = '';
   // Creating navigation bar
   const navBar = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 mb-5">
       <a class="navbar-brand" href="#">TV Shows</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
